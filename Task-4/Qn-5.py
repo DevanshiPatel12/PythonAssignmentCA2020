771c19e07b3f958b2310e9f99d5004bb4d1e9fdf
DevanshiPatel12 <63202410+DevanshiPatel12@users.noreply.github.com>
2020-04-10 17:16:54 -0400

    Qn

---
+++ Task-4/Qn-5.py
@@ -0,0 +1,14 @@
+# more about Yield, next, generator
+
+def simple_Generator():
+    yield 1
+    yield 2
+    yield 3
+
+# x is a generator object
+x = simple_Generator()
+
+# Iterating over the generator object using next
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
